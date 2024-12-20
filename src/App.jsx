b6ef5f165e4a62682a53da866c67ccb7c7626eda
WorkAccVikas <workaccvikas@gmail.com>
2024-12-20 08:53:51 +0530

    first commit in tailwind-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <>App</>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+    </>
+  );
 };
 
 export default App;
